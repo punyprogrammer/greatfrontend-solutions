cdb7e744977539bbd9b1245eae4f12f829442f65
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2026-06-03 15:43:53 +0530

    Create 47.Breadth First Search.ts

---
+++ 47.Breadth First Search.ts
@@ -0,0 +1,95 @@
+/**
+ * @param graph The adjacency list representing the graph.
+ * @param source The source node to start traversal from. Has to be a valid node if graph is non-empty.
+ * @return A BFS-traversed order of nodes.
+ */
+export default function breadthFirstSearch(
+  graph: Record<string, Array<string>>,
+  source: string,
+): Array<string> {
+  let bfsTraversal = Array<string>();
+  const visited = new Set<string>();
+  const bfsQueue = new Queue<string>();
+  if(!source) return bfsTraversal;
+  // enqueue the first node
+  visited.add(source);
+  bfsQueue.enqueue(source);
+  while(!bfsQueue.isEmpty()){
+    const currentNode = bfsQueue.dequeue()!;
+    bfsTraversal.push(currentNode);
+    // traverse through all the neighbours
+    for(let neighbour of graph[currentNode]){
+       if(!visited.has(neighbour)){
+           visited.add(neighbour);
+           bfsQueue.enqueue(neighbour);
+       }
+    }
+  }
+
+  return bfsTraversal;
+}
+
+/*  Auxiliary classes */
+
+/**
+ * A Queue class with O(1) enqueue and dequeue is provided for you.
+ * You can use it directly should you wish to.
+ *
+ * Example usage:
+ * const q = new Queue();
+ * q.enqueue('a');
+ * q.enqueue('b');
+ * q.dequeue(); //'a'
+ * q.isEmpty(); // False
+ */
+class Node<T> {
+  value: T;
+  next: Node<T> | null;
+
+  constructor(value: T) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue<T> {
+  head: Node<T> | null;
+  tail: Node<T> | null;
+  length: number;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  isEmpty(): boolean {
+    return this.length === 0;
+  }
+
+  enqueue(item: T): void {
+    const newNode = new Node(item);
+    if (this.isEmpty()) {
+      this.head = newNode;
+    } else if (this.tail) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    this.length++;
+  }
+
+  dequeue(): T | null {
+    if (this.isEmpty() || !this.head) {
+      return null;
+    } else {
+      const removedNode = this.head;
+      this.head = this.head.next;
+      removedNode.next = null;
+      this.length--;
+      if (this.isEmpty()) {
+        this.tail = null;
+      }
+      return removedNode.value;
+    }
+  }
+}
